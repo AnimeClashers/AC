4256de4bbce03f6aa62a0b056c12ed58fce56a33
Floriano Victor Larangeira Peixoto <rotc21@gmail.com>
2013-06-22 13:23:17 -0300

    Ac Client

---
+++ AC_Server/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ACServer::Application.config.secret_token = '486e5d290ccc635ef1ac89a578f5291f0bc9689969a17652d27e7e1ee1726541838081642738ee8bb0c06cc2d6f91a0905d674a23f9acf189f1a924b0155392e'
